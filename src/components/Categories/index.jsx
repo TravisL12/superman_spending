6a72ba7631ea05163d30b33d0ff6f313c2dc449d
Travis Lawrence <travis.lawrence@uber.com>
2019-07-25 23:33:59 -0700

    New files yay

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -5,19 +5,13 @@ import {
   currency,
   formatDate
 } from "utilities/date-format-utils";
+import CategoryGraph from "./CategoryGraph";
+import CategoryRow from "./CategoryRow";
 import style from "./Categories.module.scss";
 import Loading from "components/Loading";
 import categoryColors from "utilities/categoryColors";
 import { shuffle, values, keys } from "lodash";
 import qs from "query-string";
-import {
-  VictoryLabel,
-  VictoryAxis,
-  VictoryChart,
-  VictoryStack,
-  VictoryLine,
-  VictoryArea
-} from "victory";
 
 const colors = shuffle(categoryColors);
 const MONTHS_BACK = 12 * 1;
@@ -28,7 +22,6 @@ class Categories extends Component {
     isLoading: true,
     checkedCategories: {},
     graphCumulative: false,
-    graphType: "stack",
     dateRange: null
   };
 
@@ -76,92 +69,21 @@ class Categories extends Component {
       const sum =
         graphCumulative && idx > 0 ? monthSum + result.slice(-1)[0] : monthSum;
 
-      result.push(sum);
-      return result;
+      return [...result, sum];
     }, []);
   };
 
   calculateCategoryTotal = (month, year) => {
     const { categories } = this.state;
 
-    const total = values(categories).reduce((sum, { id, Transactions }) => {
-      sum += this.getTransactionSum(year, month, id);
-
-      return sum;
+    const total = values(categories).reduce((sum, { id }) => {
+      return sum + this.getTransactionSum(year, month, id);
     }, 0);
 
     return currency(total, {
       minimumFractionDigits: 0,
       rounded: true
     });
-  };
-
-  buildGraph = () => {
-    const { categories } = this.state;
-    const data = values(categories).map(({ id, name, Transactions }, idx) => {
-      return this.getMonthSums(id).map((sum, idx) => {
-        return { x: idx, y: sum / 100 };
-      });
-    });
-
-    const graphHeight = 150;
-    const axisColor = "black";
-    const axisFontSize = 6;
-
-    const stackChart = (
-      <VictoryStack colorScale={colors}>
-        {data.map((d, idx) => {
-          return <VictoryArea data={d} key={idx} />;
-        })}
-      </VictoryStack>
-    );
-
-    const lineChart = data.map((d, idx) => {
-      return (
-        <VictoryLine
-          data={d}
-          key={idx}
-          style={{
-            data: { stroke: colors[idx] }
-          }}
-        />
-      );
-    });
-
-    return (
-      <VictoryChart
-        animate={{ duration: 500 }}
-        padding={{ top: 10, bottom: 10, left: 10, right: 10 }}
-        height={graphHeight}
-      >
-        {this.state.graphType === "stack" ? stackChart : lineChart}
-
-        {/* X-axis */}
-        <VictoryAxis
-          style={{ tickLabels: { fontSize: axisFontSize } }}
-          tickCount={categories.length}
-        />
-
-        {/* Y-axis */}
-        <VictoryAxis
-          dependentAxis
-          tickFormat={t => `$${t.toLocaleString()}`}
-          tickLabelComponent={<VictoryLabel dx={35} dy={-5} />}
-          style={{
-            axis: { stroke: 0 },
-            tickLabels: {
-              fill: axisColor,
-              fontSize: axisFontSize,
-              fontWeight: 600
-            },
-            grid: {
-              strokeDasharray: "15, 5",
-              stroke: axisColor
-            }
-          }}
-        />
-      </VictoryChart>
-    );
   };
 
   handleCategoryCheckboxChange = event => {
@@ -180,8 +102,6 @@ class Categories extends Component {
 
     if (isLoading) return <Loading />;
 
-    const graph = this.buildGraph();
-
     return (
       <div className={style.categoryTransactions}>
         <div className={style.graph}>
@@ -192,21 +112,11 @@ class Categories extends Component {
           >
             Toggle Cumulative
           </button>
-          <button
-            onClick={() => {
-              this.setState({ graphType: "stack" });
-            }}
-          >
-            Stack Chart
-          </button>
-          <button
-            onClick={() => {
-              this.setState({ graphType: "line" });
-            }}
-          >
-            Line Chart
-          </button>
-          {graph}
+          <CategoryGraph
+            categories={categories}
+            colors={colors}
+            getMonthSums={this.getMonthSums}
+          />
         </div>
         <div className={style.table}>
           <table>
@@ -226,36 +136,16 @@ class Categories extends Component {
               </tr>
             </thead>
             <tbody>
-              {values(categories).map(({ id, name, Transactions }, idx) => {
-                const checkBoxStyling = checkedCategories[id]
-                  ? { background: colors[idx], color: "black" }
-                  : { background: "lightgray", color: "gray" };
-
+              {values(categories).map((category, idx) => {
                 return (
-                  <tr key={`name-${idx}`}>
-                    <td>
-                      <input
-                        type="checkbox"
-                        id={`category-${id}`}
-                        value={id}
-                        checked={checkedCategories[id]}
-                        onChange={this.handleCategoryCheckboxChange}
-                      />
-                      <label htmlFor={`category-${id}`} style={checkBoxStyling}>
-                        {name}
-                      </label>
-                    </td>
-                    {this.getMonthSums(id).map((sum, sIdx) => {
-                      return (
-                        <td className={style.amountCol} key={`cat-${sIdx}`}>
-                          {currency(sum, {
-                            rounded: true,
-                            minimumFractionDigits: 0
-                          })}
-                        </td>
-                      );
-                    })}
-                  </tr>
+                  <CategoryRow
+                    checkedCategories={checkedCategories}
+                    color={colors[idx]}
+                    category={category}
+                    getMonthSums={this.getMonthSums}
+                    onCheckboxChange={this.handleCategoryCheckboxChange}
+                    key={idx}
+                  />
                 );
               })}
               <tr>
